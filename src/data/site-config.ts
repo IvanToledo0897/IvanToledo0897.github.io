5b6b0c00d4a87b9d2ceacff9dd6b5c7e0e9074f1
IvanToledo0897 <ivantoledo0897@gmail.com>
2026-03-23 17:43:51 -0600

    Home detailes fixed and 1st project try

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -8,8 +8,8 @@ const siteConfig: SiteConfig = {
         src: avatar,
         alt: 'Ethan Donovan'
     },
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: 'Ivan',
+    subtitle: 'SQL and Python Data Analyst',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
@@ -55,7 +55,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Welcome, pleased for your visit. Explore my Data Portfolio.',
-        text: "I'm a Data Analyst with Experiences in Python, SQL, Google Sheets and C++. I love to have the full picture and analyse every aspects. \nMy portfolio talks more about me than I could possible can. C'mon, lets dive into it!",
+        text: "I'm a Data Analyst with experience in Python, SQL, Google Sheets and C++. I love to have the full picture and analyse every aspects of it. \nMy portfolio talks more about me than I could possible do. C'mon, lets dive into it!",
         //text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence.\nMy approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products.\nI have a profound appreciation for top-notch software, visual design, and the principles of product-led growth.\n\nFeel free to explore some of my coding endeavors on [GitHub](https://github.com/JustGoodUI/dante-astro-theme) or follow me on [Twitter/X](https://twitter.com/justgoodui).",
         image: {
             src: hero,

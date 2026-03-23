0f17bb14019391fcdca5ef0a8bd9b6ea2f421721
IvanToledo0897 <ivantoledo0897@gmail.com>
2026-03-23 16:05:51 -0600

    Change home page and add experience in content

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -13,7 +13,7 @@ const siteConfig: SiteConfig = {
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        alt: 'Ivan - SQL and Python Data Analyst'
     },
     headerNavLinks: [
         {
@@ -25,12 +25,8 @@ const siteConfig: SiteConfig = {
             href: '/projects'
         },
         {
-            text: 'Blog',
-            href: '/blog'
-        },
-        {
-            text: 'Tags',
-            href: '/tags'
+            text: 'Experience',
+            href: '/experience'
         }
     ],
     footerNavLinks: [
@@ -53,21 +49,14 @@ const siteConfig: SiteConfig = {
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
-        {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
-        },
-        {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
+            text: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/ivan-enrique-vazquez-toledo'
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence.\nMy approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products.\nI have a profound appreciation for top-notch software, visual design, and the principles of product-led growth.\n\nFeel free to explore some of my coding endeavors on [GitHub](https://github.com/JustGoodUI/dante-astro-theme) or follow me on [Twitter/X](https://twitter.com/justgoodui).",
+        title: 'Welcome, pleased for your visit. Explore my Data Portfolio.',
+        text: "I'm a Data Analyst with Experiences in Python, SQL, Google Sheets and C++. I love to have the full picture and analyse every aspects. \nMy portfolio talks more about me than I could possible can. C'mon, lets dive into it!",
+        //text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence.\nMy approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products.\nI have a profound appreciation for top-notch software, visual design, and the principles of product-led growth.\n\nFeel free to explore some of my coding endeavors on [GitHub](https://github.com/JustGoodUI/dante-astro-theme) or follow me on [Twitter/X](https://twitter.com/justgoodui).",
         image: {
             src: hero,
             alt: 'A person sitting at a desk in front of a computer'
@@ -80,15 +69,15 @@ const siteConfig: SiteConfig = {
         ]
     },
     subscribe: {
-        enabled: true,
+        enabled: false,
         title: 'Subscribe to Dante Newsletter',
         text: 'One update per week. All the latest posts directly in your inbox.',
         form: {
             action: '#'
         }
     },
-    postsPerPage: 8,
-    projectsPerPage: 8
+    //postsPerPage: 8,
+    projectsPerPage: 4
 };
 
 export default siteConfig;

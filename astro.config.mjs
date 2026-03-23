b783be8d58dadca2448c6abf6833c7b8a36b9343
IvanToledo0897 <ivantoledo0897@gmail.com>
2026-03-23 14:27:27 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: "https://IvanToledo0897.github.io",
     vite: {
         plugins: [tailwindcss()]
     },
